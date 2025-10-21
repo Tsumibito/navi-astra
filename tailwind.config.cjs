f1830ee2fb48d3dd3b15ccc0d12ec48b3e70c747
Alex <Alex.Tsumibito@gmail.com>
2025-10-21 16:06:05 +0200

    feat: добавить локализованный лейаут и новую навигацию

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,25 @@ module.exports = {
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-orange': '#ffb052',
+        'brand-orangeDark': 'rgba(226, 129, 15, 1)',
+        'brand-blue': 'rgba(47, 175, 215, 1)',
+      },
+      fontFamily: {
+        neo: [
+          'Inter',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial Nova',
+          'Nimbus Sans',
+          'Arial',
+          'sans-serif',
+        ],
+        tenor: ['"Tenor Sans"', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
